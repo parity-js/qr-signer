aec8f3ff6c04a474973ae8ca022725d1926c4444
maciejhirsz <maciej.hirsz@gmail.com>
2018-06-13 16:48:31 +0200

    Handle ERC-681 addresses next to legacy Parity Signer ones

--- example/src/Example.js
+++ example/src/Example.js
@@ -4,14 +4,14 @@ import QrSigner from './dist'
 export default class Example extends Component {
   state = {
     scan: false,
-    signature: ''
+    scanned: null,
   }
 
   render () {
-    const { scan, signature } = this.state;
+    const { scan, scanned } = this.state;
 
-    if (signature) {
-      return <div>Signature: {signature}</div>;
+    if (scanned) {
+      return <div>Scanned: {JSON.stringify(scanned)}</div>;
     }
 
     return (
@@ -21,7 +21,7 @@ export default class Example extends Component {
           scan={scan}
           account='0x007311b88A03af17dbb37B47ab7C9Ab556708D56'
           rlp='0xeb808504a817c8008252089400255cf193f1ba6dd3ec08ebe62e393030f4dd34872386f26fc10000802a8080'
-          onScan={(signature) => this.setState({ signature })}
+          onScan={(scanned) => this.setState({ scanned })}
         />
         <button onClick={() => this.setState({ scan: !this.state.scan })}>Toggle Scan</button>
       </div>
